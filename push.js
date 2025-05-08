bc9c0e5dcc48f93db40e5d78c8569d9f29515aa9
Juan Guevara M. <juanexgue@gmail.com>
2025-05-07 22:14:04 -0500

    agregar un  elemento a un array

---
+++ push.js
@@ -0,0 +1,10 @@
+// Methods that modify the original array (Mutability)
+
+// push()
+
+const countries = ['USA', 'Canada', 'UK']
+console.log(countries)
+const newCountries = countries.push('Germany','Australia')
+
+console.log(countries)
+console.log(newCountries) // Devuelve un valor
