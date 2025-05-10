fc0648aeb3a0baa6a67e22d32ac7b106ad6974c8
Juan Guevara M. <juanexgue@gmail.com>
2025-05-09 19:37:59 -0500

    Indice my projects

--- main.js
+++ main.js
@@ -4,20 +4,17 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
+  <h1>My projects</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="post.html" target="_blank" class="logo"> 
+      POST
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+  </div>
+  <br/>
+    <div>
+    <a href="eventos.html" target="_blank" class="logo"> 
+      EVENTOS
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
